244a72cf6c01b0e2fe55c2e602396825b8699224
estebanrivas720 <estebanrivas720@gmail.com>
2022-05-30 15:57:53 -0400

    Index básico

--- index.php
+++ index.php
@@ -11,10 +11,16 @@
     <h2>Página web </h2>
     <a href="./Registro.php">Registrar Usuario</a></br>
     <a href="./InicioSesion.php">Iniciar sesión</a></br>
+    <a href="./CerrarSesion.php">Cerrar sesión</a></br>
     <?php
     session_start();
+    if(isset($_SESSION['nombre'])){
         echo "Sesión actual: ", $_SESSION['nombre'];?></br><?php
         echo "Rut: ", $_SESSION['rut'];
+    }else{
+        echo "No hay una sesión iniciada.";
+    }
+        
     ?>
 </body>
 </html>
